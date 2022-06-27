2ea2e177a9903468a6e08b7cd5d9474eb625d716
Janshen Vernon <janshen.vbb@gmail.com>
2022-06-27 16:51:03 -0300

    added clean table and tableItem component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TableComponent } from './components/table/table.component';
+import { TableItemComponent } from './components/table-item/table-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TableComponent,
+    TableItemComponent
   ],
   imports: [
     BrowserModule,
